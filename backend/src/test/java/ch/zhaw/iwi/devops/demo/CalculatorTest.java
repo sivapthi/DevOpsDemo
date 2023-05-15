776aa139d2f3f8b7ba2d5c36262b6c620436e5a0
Thinesan Sivapathasundaram <sivapthi@students.zhaw.ch>
2023-05-15 20:32:51 +0200

    Test Add1, Sub1, Mult1, Div1, Mult2, Div2 erstellt

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/CalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/CalculatorTest.java
@@ -52,4 +52,76 @@ public class CalculatorTest {
 
         assertEquals(expectedQuotient, actualQuotient);
     }
+
+    @Test
+    public void TestAddition1(){
+        Calculator calculator = new Calculator();
+        int num1 = 7;
+        int num2 = 7;
+        int expectedSum = 14;
+
+        int actualSum = calculator.add(num1, num2);
+  
+        assertEquals(expectedSum, actualSum);
+    }
+    
+    @Test
+    public void TestSubtraktion1(){
+        Calculator calculator = new Calculator();
+        int num1 = 10;
+        int num2 = 5;
+        int expectedDifference = 5;
+
+        int actualDifference = calculator.subtract(num1, num2);
+        
+        assertEquals(expectedDifference, actualDifference);
+    }
+
+    @Test
+    public void TestMultiplikation1(){
+        Calculator calculator = new Calculator();
+        int num1 = 10;
+        int num2 = 5;
+        int expectedProduct = 50;
+
+        int actualProduct = calculator.multiply(num1, num2);
+
+        assertEquals(expectedProduct, actualProduct);        
+    }
+
+    @Test
+    public void TestDivision1(){
+        Calculator calculator = new Calculator();
+        int num1 = 10;
+        int num2 = 5;
+        int expectedQuotient = 2;
+
+        int actualQuotient = calculator.divide(num1, num2);
+
+        assertEquals(expectedQuotient, actualQuotient);
+    }
+
+    @Test
+    public void TestMultiplikation2(){
+        Calculator calculator = new Calculator();
+        int num1 = 20;
+        int num2 = 6;
+        int expectedProduct = 120;
+
+        int actualProduct = calculator.multiply(num1, num2);
+
+        assertEquals(expectedProduct, actualProduct);        
+    }
+
+    @Test
+    public void TestDivision2(){
+        Calculator calculator = new Calculator();
+        int num1 = 60;
+        int num2 = 10;
+        int expectedQuotient = 6;
+
+        int actualQuotient = calculator.divide(num1, num2);
+
+        assertEquals(expectedQuotient, actualQuotient);
+    }
 }
